6fe2a41cae242776a3cf695008bdfdfcbda2f4c1
Alexander Poselentsev <as.poselentsev@gmail.com>
2025-05-03 06:02:39 +0300

    create simple deck

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,68 @@ function App() {
 
   return (
     <>
-      <div>Start</div>
+      <div className="card__list">
+        <div className="card__heart">
+          <div className="card">♥ 1</div>
+          <div className="card">♥ 2</div>
+          <div className="card">♥ 3</div>
+          <div className="card">♥ 4</div>
+          <div className="card">♥ 5</div>
+          <div className="card">♥ 6</div>
+          <div className="card">♥ 7</div>
+          <div className="card">♥ 8</div>
+          <div className="card">♥ 9</div>
+          <div className="card">♥ 10</div>
+          <div className="card">♥ J</div>
+          <div className="card">♥ Q</div>
+          <div className="card">♥ K</div>
+        </div>
+        <div className="card__spade">
+          <div className="card">♠ 1</div>
+          <div className="card">♠ 2</div>
+          <div className="card">♠ 3</div>
+          <div className="card">♠ 4</div>
+          <div className="card">♠ 5</div>
+          <div className="card">♠ 6</div>
+          <div className="card">♠ 7</div>
+          <div className="card">♠ 8</div>
+          <div className="card">♠ 9</div>
+          <div className="card">♠ 10</div>
+          <div className="card">♠ J</div>
+          <div className="card">♠ Q</div>
+          <div className="card">♠ K</div>
+        </div>
+        <div className="card__diamond">
+          <div className="card">♦ 1</div>
+          <div className="card">♦ 2</div>
+          <div className="card">♦ 3</div>
+          <div className="card">♦ 4</div>
+          <div className="card">♦ 5</div>
+          <div className="card">♦ 6</div>
+          <div className="card">♦ 7</div>
+          <div className="card">♦ 8</div>
+          <div className="card">♦ 9</div>
+          <div className="card">♦ 10</div>
+          <div className="card">♦ J</div>
+          <div className="card">♦ Q</div>
+          <div className="card">♦ K</div>
+        </div>
+        <div className="card__club">
+          <div className="card">♣ 1</div>
+          <div className="card">♣ 2</div>
+          <div className="card">♣ 3</div>
+          <div className="card">♣ 4</div>
+          <div className="card">♣ 5</div>
+          <div className="card">♣ 6</div>
+          <div className="card">♣ 7</div>
+          <div className="card">♣ 8</div>
+          <div className="card">♣ 9</div>
+          <div className="card">♣ 10</div>
+          <div className="card">♣ J</div>
+          <div className="card">♣ Q</div>
+          <div className="card">♣ K</div>
+        </div>
+      </div>
     </>
   )
 }
