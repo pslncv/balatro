0e31217c35ad3768f1693f96c8c347d978b20516
Alexander Poselentsev <as.poselentsev@gmail.com>
2025-05-03 15:30:21 +0300

    feat: [#02] implement hand state and scoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,58 @@
+import { useState, useEffect } from 'react'
+import { Card } from './types/card'
 import './style.scss'
 
 function App() {
 
+  // Что в руке
+  const [hand, setHand] = useState<Card[]>([])
+  // Очки
+  const [score, setScore] = useState<number>(0)
+
+  // Функция подсчёта очков
+  const calculateScore = (cards: Card[]): number => {
+    return cards.reduce((acc, card) => acc + card.value, 0);
+  }
+
+  // Функция создание сущности карты
+  // Оборачиваем в (), чтобы сказать интерпретатору: "Я возвращаю объект, а не блок кода".
+  const createCard = ():Card => ({
+    id: Date.now(),
+    rank: 'A',
+    suit: 'spades',
+    value: 11,
+  })
+
+  // Добавление карты в руку
+  const addCard = () => {
+    if (hand.length < 5) {
+      // Добавляем её в состояние руки
+      setHand(prev => [...prev, createCard()])
+    } else alert('В руке 5 карт')
+  }
+
+  // Как только рука изменилась, сразу считаем очки
+  useEffect(() => {
+    const newScore = calculateScore(hand)
+    // И устанавливаем в состояние очков
+    setScore(newScore)
+  }, [hand]);
+
   return (
     <>
-      <div className="card__list">
-        <div className="card__heart">
-          <div className="card">♥ 1</div>
-          <div className="card">♥ 2</div>
-          <div className="card">♥ 3</div>
-          <div className="card">♥ 4</div>
-          <div className="card">♥ 5</div>
-          <div className="card">♥ 6</div>
-          <div className="card">♥ 7</div>
-          <div className="card">♥ 8</div>
-          <div className="card">♥ 9</div>
-          <div className="card">♥ 10</div>
-          <div className="card">♥ J</div>
-          <div className="card">♥ Q</div>
-          <div className="card">♥ K</div>
-        </div>
-        <div className="card__spade">
-          <div className="card">♠ 1</div>
-          <div className="card">♠ 2</div>
-          <div className="card">♠ 3</div>
-          <div className="card">♠ 4</div>
-          <div className="card">♠ 5</div>
-          <div className="card">♠ 6</div>
-          <div className="card">♠ 7</div>
-          <div className="card">♠ 8</div>
-          <div className="card">♠ 9</div>
-          <div className="card">♠ 10</div>
-          <div className="card">♠ J</div>
-          <div className="card">♠ Q</div>
-          <div className="card">♠ K</div>
-        </div>
-        <div className="card__diamond">
-          <div className="card">♦ 1</div>
-          <div className="card">♦ 2</div>
-          <div className="card">♦ 3</div>
-          <div className="card">♦ 4</div>
-          <div className="card">♦ 5</div>
-          <div className="card">♦ 6</div>
-          <div className="card">♦ 7</div>
-          <div className="card">♦ 8</div>
-          <div className="card">♦ 9</div>
-          <div className="card">♦ 10</div>
-          <div className="card">♦ J</div>
-          <div className="card">♦ Q</div>
-          <div className="card">♦ K</div>
-        </div>
-        <div className="card__club">
-          <div className="card">♣ 1</div>
-          <div className="card">♣ 2</div>
-          <div className="card">♣ 3</div>
-          <div className="card">♣ 4</div>
-          <div className="card">♣ 5</div>
-          <div className="card">♣ 6</div>
-          <div className="card">♣ 7</div>
-          <div className="card">♣ 8</div>
-          <div className="card">♣ 9</div>
-          <div className="card">♣ 10</div>
-          <div className="card">♣ J</div>
-          <div className="card">♣ Q</div>
-          <div className="card">♣ K</div>
-        </div>
+      <h1>Score: {score}</h1>
+      <button onClick={addCard}>add card</button>
+      <div className='card__list'>
+        {/* Отображаем руку в реальном времени */}
+        {hand.map((card) => (
+          <div key={card.id} className="card">
+            {card.rank} of {card.suit}
+          </div>
+        ))}
       </div>
     </>
   )
 }
+
 
 export default App
---
+++ src/types/card.ts
@@ -0,0 +1,6 @@
+export interface Card {
+    id: number;
+    rank: string; // Туз ('A'), Король ('K'), etc.
+    suit: string; // Черви ('Hearts), Пики ('Spades'), etc.
+    value: number; // A - 11, K - 10, 2 - 2, etc.
+}
